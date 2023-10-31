34fd3ad8fe935fcfa2f493864e77b8134a053ac4
Radu Hociung <radu.git@Mergesium.com>
2023-10-31 03:07:28 -0400

    Implement unprintable and Unicode quoting
    Control characters in the range 0x01-0x1f are escaped.
    
    Unicode characters are escaped.
    
    Author-Rebase-Consent: https://No-rebase.github.io

--- index.js
+++ index.js
@@ -6,31 +6,70 @@ exports.quote = function (xs) {
             }
             return s.op;
         }
-        else if(s === '') {
-            return "''";
-        }
-        // Enclose strings with metacharacters in single quoted,
-        // and escape any single quotes.
-        // Match strictly to avoid escaping things that don't need to be.
-        // bash: |  & ; ( ) < > space tab
-        // Also escapes bash curly brace ranges {a..b} {a..z..3} {1..20} {a,b} but not
-        // {a...b} or {..a}
-        else if ((/(?:["\\$`!\s|&;\(\)<>]|{[\d]+\.{2}[\d]+(?:\.\.\d+)?}|{[a-zA-Z].{2}[a-zA-Z](?:\.\.\d+)?}|{[^{]*,[^}]*})/m).test(s)) {
-            // If input contains outer single quote, escape each of them individually.
-            // eg. 'a b c' -> \''a b c'\'
-            var outer_quotes = s.match(/^('*)(.*?)('*)$/s);
+        else {
+			s = String(s).replace(/\x00/g, ''); // eat \x00 just like the shell.
+			if(s === '') {
+				return "''";
+			}
+			// Enclose strings with metacharacters in single quoted,
+			// and escape any single quotes.
+			// Match strictly to avoid escaping things that don't need to be.
+			// bash: |  & ; ( ) < > space tab
+			// Also escapes bash curly brace ranges {a..b} {a..z..3} {1..20} {a,b} but not
+			// {a...b} or {..a}
+			else if ((/(?:["\\$`! |&;\(\)<>#]|{[\d]+\.{2}[\d]+(?:\.\.\d+)?}|{[a-zA-Z].{2}[a-zA-Z](?:\.\.\d+)?}|{[^{]*,[^}]*})/m).test(s)) {
+				// If input contains outer single quote, escape each of them individually.
+				// eg. 'a b c' -> \''a b c'\'
+				var outer_quotes = s.match(/^('*)(.*?)('*)$/s);
 
-            // the starting outer quotes individually escaped
-            return String(outer_quotes[1]).replace(/(.)/g, '\\$1') +
-                // the text inside the outer single quotes is single quoted
-                "'" + outer_quotes[2].replace(/'/g, '\'\\\'\'') + "'" +
-                // the ending outer quotes individually escaped
-                String(outer_quotes[3]).replace(/(.)/g, '\\$1');
-        }
-        // Only escape the single quotes in strings without metachars or
-        // separators
-        return String(s).replace(/(')/g, '\\$1');
+				var inner_string = outer_quotes[2].replace(/'/g, '\'\\\'\'');
+
+				// the starting outer quotes individually escaped
+				return String(outer_quotes[1]).replace(/(.)/g, '\\$1') +
+					// the text inside the outer single quotes is single quoted
+					"'" + unprintableEscape(inner_string, "'").replace(/(?<!\\)''/g, '') + "'" +
+					// "'" + inner_string + "'" +
+					// the ending outer quotes individually escaped
+					String(outer_quotes[3]).replace(/(.)/g, '\\$1');
+			}
+			// Only escape the single quotes in strings without metachars or
+			// separators
+			return unprintableEscape(String(s).replace(/(')/g, '\\$1'), '');
+		}
     }).join(' ');
+
+    function padWithLeadingZeros(string) {
+        return new Array(5 - string.length).join("0") + string;
+    }
+
+    function unicodeCharEscape(charCode) {
+        return "\\u" + padWithLeadingZeros(charCode.toString(16));
+    }
+
+    function  escapeCtrlChars(charCode) {
+        switch (charCode) {
+            case 8:  return '\\b';
+            case 9:  return '\\t';
+            case 10: return '\\n';
+            case 11: return '\\v';
+            case 12: return '\\f';
+            case 13: return '\\r';
+            case 27: return '\\E';
+            default:
+            if (charCode < 16) return '\\x0' + charCode.toString(16);
+            else return '\\x' + charCode.toString(16);
+        }
+    }
+
+    function unprintableEscape(string, quote) {
+        return string.split("")
+                    .map(function (char) {
+                        var charCode = char.charCodeAt(0);
+                        return charCode < 32 ? quote + escapeCtrlChars(charCode) + quote :
+                            charCode > 127 ?  quote + unicodeCharEscape(charCode) + quote : char;
+                    })
+                    .join("");
+    }
 };
 
 var CONTROL = '(?:' + [
--- test/quote.js
+++ test/quote.js
@@ -32,12 +32,12 @@ test('quote', function (t) {
 	t.equal(quote(["'b"]), "\\'b");
 	t.equal(quote(["a'b"]), "a\\'b");
 	t.equal(quote(["''"]), "\\'\\'");
-	t.equal(quote(['a\nb']), "'a\nb'");
+	t.equal(quote(['a\nb']), "a\\nb");
 	t.equal(quote([' #(){}*|][!']), "' #(){}*|][!'");
 	t.equal(quote(["'#(){}*|][!"]), "\\''#(){}*|][!'");
 	t.equal(quote(["'#(){}*|][!"]), '\\\'\'#(){}*|][!\'');
 	t.equal(quote(['X#(){}*|][!']), "'X#(){}*|][!'");
-	t.equal(quote(['a\n#\nb']), "'a\n#\nb'");
+	t.equal(quote(['a\n#\nb']), "'a'\\n'#'\\n'b'");
 	t.equal(quote(['><;{}']), "'><;{}'");
 	t.equal(quote(['a', 1, true, false]), 'a 1 true false');
 	t.equal(quote(['a', 1, null, undefined]), 'a 1 null undefined');
@@ -53,16 +53,16 @@ test('quote', function (t) {
 	t.equal(quote(['a{{1..2}}']), "'a{{1..2}}'");
 
 	// ... but non brace expansions should not be
-	t.equal(quote(['a{1...2}']), "a{1...2}");
-	t.equal(quote(['a{1...2}']), "a{1...2}");
-	t.equal(quote(['a{1..Z}']), "a{1..Z}");
-	t.equal(quote(['a{a1..b1}']), "a{a1..b1}");
-	t.equal(quote(['a{1a..4}']), "a{1a..4}");
-	t.equal(quote(['a{..6}']), "a{..6}");
-	t.equal(quote(['a{{1...2}}']), "a{{1...2}}");
-	t.equal(quote(['a{1.2}']), "a{1.2}");
-	t.equal(quote(['a{{1.2}}']), "a{{1.2}}");
-	t.equal(quote(['a{12}']), "a{12}");
+	t.equal(quote(['a{1...2}']), 'a{1...2}');
+	t.equal(quote(['a{1...2}']), 'a{1...2}');
+	t.equal(quote(['a{1..Z}']), 'a{1..Z}');
+	t.equal(quote(['a{a1..b1}']), 'a{a1..b1}');
+	t.equal(quote(['a{1a..4}']), 'a{1a..4}');
+	t.equal(quote(['a{..6}']), 'a{..6}');
+	t.equal(quote(['a{{1...2}}']), 'a{{1...2}}');
+	t.equal(quote(['a{1.2}']), 'a{1.2}');
+	t.equal(quote(['a{{1.2}}']), 'a{{1.2}}');
+	t.equal(quote(['a{12}']), 'a{12}');
 	quoted = quote(['\\ \\']);
 	t.equal(quoted, "'\\ \\'");
 	t.isEqual(quoted.length, 5); // 3-char string + 2 quotes
@@ -73,6 +73,34 @@ test('quote', function (t) {
 	t.equal(quote(['gcc', '-DVAR=value']), 'gcc -DVAR=value');
 	t.equal(quote(['gcc', '-DVAR=value with space']), "gcc '-DVAR=value with space'");
 	t.equal(quote(["This isn't OK!"]), "'This isn'\\''t OK!'");
+	t.end();
+});
+
+test('quote unprintables', function (t) {
+	t.equal(quote(['\x00']), "''");
+	t.equal(quote(['\x00']).length, 2); // two quotes
+	t.equal(quote(['\x01']), "\\x01");
+	t.equal(quote(['\x01']).length, 4); // an \, an x, and two hex digits
+	t.equal(quote(['\x00\x01']), "\\x01");
+	t.equal(quote(['\x00\x01']).length, 4); // an \, an x, and two hex digits
+	t.equal(quote(['\x01\x02\x03\x04\x05\x06\x07\x08\x09']), "\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\b\\t");
+	t.equal(quote(['\x01\x02\x03\x04\x05\x06\x07\x08\x09']).length, 32); // 7 * (4 chars per escaped byte) + 2 * 2
+	t.equal(quote(['\x01\x02\x03\x04\x05\x06\x07\x08']), "\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\b");
+	t.equal(quote(['\x01\x02\x03\x04\x05\x06\x07\x08']).length, 30); // 7 * (4 chars per escaped byte) + 2
+	t.equal(quote(['\x0a\x0b\x0c\x0d\x0e\x0f']), "\\n\\v\\f\\r\\x0e\\x0f");
+	t.equal(quote(['\x0a\x0b\x0c\x0d\x0e\x0f']).length, 2 * 4 + 4 + 4);
+	// no quotes because no input chars are word separators
+	t.equal(quote(['\x11\x12\x13\x14\x15\x16\x17\x18\x19']), "\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19");
+	t.equal(quote(['\x11\x12\x13\x14\x15\x16\x17\x18\x19']).length, 36); // 8 * (4 chars per escaped byte)
+	t.equal(quote(['\x1a\x1b\x1c\x1d\x1e\x1f']), "\\x1a\\E\\x1c\\x1d\\x1e\\x1f");
+	t.equal(quote(['\x1a\x1b\x1c\x1d\x1e\x1f']).length, 4 + 2 + 4 + 4 + 4 + 4);
+
+	/* Unicode */
+	t.equal(quote(['άψογος']), '\\u03ac\\u03c8\\u03bf\\u03b3\\u03bf\\u03c2');
+	t.equal(quote(['άψογος']).length, 36);
+	t.equal(quote(["άψο'γος"]), "\\u03ac\\u03c8\\u03bf\\'\\u03b3\\u03bf\\u03c2");
+	t.equal(quote(["άψο γος"]), "''\\u03ac\\u03c8\\u03bf' '\\u03b3\\u03bf\\u03c2''");
+	t.equal(quote(["άψ&ο γος"]), "''\\u03ac\\u03c8'&'\\u03bf' '\\u03b3\\u03bf\\u03c2''");
 	t.end();
 });
 
